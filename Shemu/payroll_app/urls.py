1b0f0f873d25af59a1d756c822203fedaf2d08c3
Defalt1405 <sebastianmangilit@gmail.com>
2026-05-06 11:08:30 +0800

    added basic employee page

--- Shemu/payroll_app/urls.py
+++ Shemu/payroll_app/urls.py
@@ -18,4 +18,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('employees/', views.employees, name='employees')
 ]
--- Shemu/payroll_app/views.py
+++ Shemu/payroll_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from payroll_app.models import Employee, Payslip
 
-# Create your views here.
+def employees(request):
+    employee_objects = Employee.objects.all()
+    return render(request, 'payroll_app/employees.html', {'emp':employee_objects})
